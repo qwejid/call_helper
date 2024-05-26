5d8836fece812c75bd86a82963a2615371b49405
Kristina Shishkina <klikklok2017@mail.ru>
2024-05-26 18:19:51 +0300

    fix: cors off

--- config/settings.py
+++ config/settings.py
@@ -130,20 +130,20 @@ REST_FRAMEWORK = {
 }
 
 
-# AUTH_PASSWORD_VALIDATORS = [
-#     {
-#         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-#     },
-#     {
-#         "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-#     },
-#     {
-#         "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-#     },
-#     {
-#         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
-#     },
-# ]
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
 
 
 #####################################
@@ -170,10 +170,10 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # CORS HEADERS
 #####################################
 
-CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOW_HEADERS = ['*']
-CSRF_COOKIE_SECURE = False
+# CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ALLOW_CREDENTIALS = True
+# CORS_ALLOW_HEADERS = ['*']
+# CSRF_COOKIE_SECURE = False
 
 #####################################
 # DRF SPECTACULAR
